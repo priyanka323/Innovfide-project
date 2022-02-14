d5c9210bd4bf6bbdc7328d50e25fa33be397e5d5
priyanka323 <priyarankawat88160@gmail.com>
2022-02-13 19:51:29 -0800

    innovfide

---
+++ script.js
@@ -0,0 +1,4 @@
+function contactForm()
+{
+    document.getElementById("contact").innerHTML = "<form><input type='text' name='name' placeholder='Name'><br><input type='text' name='email' placeholder='Email'><br><textarea name='message' placeholder='Message'></textarea><br><input type='submit' value='Submit'></form>";
+}
